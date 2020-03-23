f965f471bdd413e65353936a1fb48b446c56c04e
Nikko Aldrich Monroe <monroenikkoaldrich@yahoo.com>
2020-03-23 23:57:13 +0800

    implement auth using policy

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -8,6 +8,9 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -70,6 +73,7 @@ class QuestionController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize('update', $question);
         return view('questions.edit', compact('question'));
     }
 
@@ -82,6 +86,8 @@ class QuestionController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize('update', $question);
+
         $question->update($request->only('title', 'body'));
         return redirect('/questions')->with('success', 'Your question has been updated.');
     }
@@ -94,6 +100,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize('delete', $question);
         $question->delete();        
         return redirect('/questions')->with('success', 'Your question has been deleted.');
     }
--- resources/views/questions/_form.blade.php
+++ resources/views/questions/_form.blade.php
@@ -1,7 +1,7 @@
 @csrf
 <div class="form-group">
     <label for="question-title">Question Title</label>
-    <input type="text" name="title" value="{{ old('title', $question->title)}}" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid': ''}}">
+    <input type="text" name="title" value="{{ old('title', $questions->title)}}" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid': ''}}">
     @if($errors->has('title'))
         <div class="invalid-feedback">
             <strong>{{ $errors->first('title')}}</strong>
@@ -10,7 +10,7 @@
 </div>
 <div class="form-group">
     <label for="question-title">Explain your question</label>
-    <textarea type="text" rows="7" name="body" id="question-body" row="10" class="form-control {{ $errors->has('body') ? 'is-invalid': ''}}">{{ old('body', $question->body)}}</textarea>
+    <textarea type="text" rows="7" name="body" id="question-body" row="10" class="form-control {{ $errors->has('body') ? 'is-invalid': ''}}">{{ old('body', $questions->body)}}</textarea>
     @if($errors->has('body'))
         <div class="invalid-feedback">
             <strong>{{ $errors->first('body')}}</strong>
