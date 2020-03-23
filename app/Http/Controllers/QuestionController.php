0e6092cc98f5cedf383616160212804b1037c868
Nikko Aldrich Monroe <monroenikkoaldrich@yahoo.com>
2020-03-23 23:27:43 +0800

    implement auth using gates

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -70,7 +70,10 @@ class QuestionController extends Controller
      */
     public function edit(Question $question)
     {
-        return view('questions.edit', compact('question'));
+        if(\Gate::denies('update-question', $question)){
+            abort(403, 'Access denied');        
+        }
+        return view('questions.edit', compact('question'));        
     }
 
     /**
@@ -82,6 +85,9 @@ class QuestionController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        if(\Gate::denies('update-question', $question)){
+            abort(403, 'Access denied');        
+        }
         $question->update($request->only('title', 'body'));
         return redirect('/questions')->with('success', 'Your question has been updated.');
     }
@@ -94,6 +100,9 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
+        if(\Gate::denies('delete-question', $question)){
+            abort(403, 'Access denied');        
+        }
         $question->delete();        
         return redirect('/questions')->with('success', 'Your question has been deleted.');
     }
